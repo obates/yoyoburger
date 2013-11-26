9a78e2110164e352de24af52c3fef21ac8dc01c1
Oli Bates <olibates@live.com>
2013-11-26 17:46:12 +0000

    Initial commit, rails generate new --no-test-framework

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Yoyoburger::Application.config.secret_key_base = 'd22c3476efe5efff651ae1e0bf0ece96e6b8d93d57565b1a31a5d8c00578938815126fccaa2b97b50fde4e5017c1db84fed970dd7edea753fc16552a2c07ceab'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Yoyoburger::Application.config.session_store :cookie_store, key: '_yoyoburger_session'
